ad62f32c70dd3bb1cb9d9a6b1e051e0ac82283be
louidor-love <louidorlove@gmail.com>
2025-03-19 17:03:10 -0300

    ejercicio1

---
+++ exercice1.py
@@ -0,0 +1,9 @@
+
+# listar las claves dentro de un diccionario
+# listar los valores dentro de un diccionario
+
+team = {9:'lewan', 10:'Ansu fati', 11:'raphinha', 19:'yamal', 6: "gavi"}
+
+
+print(team.keys())
+print(team.values())
