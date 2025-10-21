b39aa6ed8ac7ac35e035960ada6d1ef552de967f
Louidor Love <louidorlove@gmail.com>
2025-10-21 19:42:46 -0300

    nuevo....

--- exercice14_poo.py
+++ exercice14_poo.py
@@ -11,18 +11,28 @@ class Usuario:
         self.palabra_input = palabra_input
 
     def __str__(self):
-        return f" nombre de usuario {self.nombre}"   
+        return f"nombre de usuario {self.nombre}"   
 
 
-usuario = Usuario(palabra_input)
-nombre = Usuario(palabra_input)
-
-
-class chatbot:
-    def __init__(self,usuario,nombre):
-        self.usuario = usuario
+class chatbot(Usuario):
+    def __init__(self,palabra_input, nombre):
+        super().__init__(palabra_input, nombre)
+        self.palabra_input = palabra_input
         self.nombre = nombre
 
     def saludar(self):
         return f"buenos dias {self.nombre} espero q tuviste un buen dia"    
+    def inicio(self):
+        return "Ahora vamos a necesitar q ingreses algo,lo q sea,nosotros nos en cargamos d decirte cual es el sentimiento "  
+    
+    def analizar():
+        pass
 
+    def responder():
+        pass
+
+nombre = Usuario(palabra_input, nombre)
+nombre = nombre.nombre    
+
+print(chatbot(palabra_input,nombre).saludar())
+print(chatbot(palabra_input,nombre).inicio())
