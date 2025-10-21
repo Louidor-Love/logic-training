6cb8268255894737c804267d23ccc367c55d64f2
Louidor Love <louidorlove@gmail.com>
2025-10-21 16:17:17 -0300

    nuevo...

--- exercice14_poo.py
+++ exercice14_poo.py
@@ -2,16 +2,27 @@
 #desarrolla un chatbot que nos pida que le digamos algo y 
 #tome eso q le decimos,analice el sentimiento y ns respnda cual es el sentimiento
 
-palabra_input = input("Ingrese algo: ")
+nombre = input("ingresee tu nombre: ")
+palabra_input = input("Ingrese tus palabras: ")
 
 class Usuario:
-    def __init__(self, palabra_input):
+    def __init__(self, palabra_input, nombre):
+        self.nombre = nombre
         self.palabra_input = palabra_input
+
+    def __str__(self):
+        return f" nombre de usuario {self.nombre}"   
 
 
 usuario = Usuario(palabra_input)
+nombre = Usuario(palabra_input)
+
 
 class chatbot:
-    def __init__(self,usuario):
+    def __init__(self,usuario,nombre):
         self.usuario = usuario
+        self.nombre = nombre
+
+    def saludar(self):
+        return f"buenos dias {self.nombre} espero q tuviste un buen dia"    
 
